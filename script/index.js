682881191dac807a060a8691ecdc526a35f7a9cc
oyintanda-zongwana <oyintandazongone@gmail.com>
2024-06-07 14:13:12 +0200

    js

---
+++ script/index.js
@@ -0,0 +1,22 @@
+let todoList = [
+    {
+        id: 1,
+        name: "Buy groceries",
+        createdDate: new Date("2024-06-07"),
+        completed: false
+    },
+    {
+        id: 2,
+        name: "Finish report",
+        createdDate: new Date("2024-06-05"),
+        completed: true
+    },
+    {
+        id: 3,
+        name: "Go to the gym",
+        createdDate: new Date("2024-06-06"),
+        completed: false
+    }
+];
+
+console.log(todoList);
